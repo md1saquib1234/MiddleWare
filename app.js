5468c168ca549bbdfdd7274d7601bf2c1f0b7589
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-03-08 01:17:49 +0530

    logger middleware

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const app = express();
 
+// app.use( (req, res, next) => {
+//   console.log("Hi, I am 1st middleware");
+//   return next();
+//   console.log("This is after next")
+// });
+
+// app.use( (req, res, next) => {
+//   console.log("Hi, I am the 2nd middleware");
+//   next();
+// });
+
+//logger middleware
+app.use((req, res, next) => {
+  req.time = new Date(Date.now()).toString();
+  console.log(req.method, req.hostname, req.path, req.time);
+  next();
+});
+
+
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.send("Hi, I am the root");
 });
+
+
+app.get("/random", (req, res) => {
+  res.send("This is a random page");
+});
+
+
+
 
 
 
